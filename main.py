b0c12fe8c8d344754bf81827366259ea75483202
F1ghtKiln <runescapeemail98@gmail.com>
2024-09-16 19:37:04 -0700

    Added Shots, that was a nightmare

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import *
-from circleshape import CircleShape
+from shot import Shot
 
 
 def main():
@@ -15,13 +15,16 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     AsteroidField.containers = (updatable)
     Asteroid.containers = (asteroids, updatable, drawable)
     Player.containers = (updatable, drawable)
+    Shot.containers = (shots,updatable,drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid = AsteroidField()
+    
 
     dt = 0
 
